763583b9092843990e362608f27c5a63ecd9461e
Rami <rami.ashraf2000@gmail.com>
2023-10-13 17:40:54 +0300

    base

---
+++ main.js
@@ -0,0 +1,24 @@
+// var placeToHelpPeople = document.getElementById("placeToHelpPeople");
+// var Places = ["Gaza", "The West Bank"];
+// var random = Math.random() * Places.length
+
+// // Function to change the text with a smooth transition
+// function changeTextWithTransition() {
+//     var r = Math.random() * Places.length
+//     if(r != random){
+//         changeTextWithTransition();
+//         return;
+//     }
+//     placeToHelpPeople.style.opacity = 0;
+//     setTimeout(function () {
+//         const randomIndex = Math.floor(random);
+//         placeToHelpPeople.textContent = Places[randomIndex];
+//         placeToHelpPeople.style.opacity = 1;
+//     }, 500); // Wait for the transition (0.5s) to complete before changing the text
+// }
+
+// // Change the text initially
+// changeTextWithTransition();
+
+// // Set an interval to change the text every 2 seconds with a smooth transition
+// setInterval(changeTextWithTransition, 2000);
